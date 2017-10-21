05657042db7b0961de4b0fa8bfd4c5b00daa41c8
RMHonor <me@r-honor.uk>
2017-10-21 23:11:41 +0100

    Initial structure to display all players

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,43 @@
 import { getAllPlayers, getPlayerById } from '../../lib/api/get';
+import { createElement, createElementTree } from '../../lib/dom/render';
+import select from '../../lib/dom/select';
 
-(function () {
-    getAllPlayers().then(data => {
-        console.log(data)
-    });
+(async function () {
+    const data = await getAllPlayers();
+
+    console.log(data);
+
+    const playerTree = createPlayerTree(data.players);
+
+    createElementTree(playerTree, select('.container'));
 })();
+
+function createPlayerTree(players) {
+    const tree = [];
+
+    players.forEach(({player}) => {
+        tree.push({
+            tagName: 'div',
+            attributes: [
+                { key: 'class', value: 'col-lg-3 col-md-4 col-sm-6 col-xs-12' }
+            ],
+            children: [
+                {
+                    tagName: 'div',
+                    attributes: [
+                        { key: 'class', value: `player-tile player-tile__image--${player.id}`}
+                    ],
+                },
+                {
+                    tagName: 'span',
+                    text: `${player.name.first} ${player.name.last}`,
+                    attributes: [
+                        { key: 'class', value: 'player-tile__name'}
+                    ]
+                },
+            ]
+        });
+    });
+
+    return tree;
+}
